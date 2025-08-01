eb5a08c314c07322e8c8ee122c4b7843c2770d17
Timmy Bui <Billie0903>
2025-08-01 14:55:10 +0700

    Update API configuration for production deployment

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { authStore } from '../store/auth'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json'
   }
